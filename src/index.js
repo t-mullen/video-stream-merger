37f1d089df474efa21d8dde299516f6ddb7b8069
RationalCoding <thomasmullendesign@gmail.com>
2018-12-07 21:33:03 -0500

    iFrames won't load videos with zero pixel dimensions

--- src/index.js
+++ src/index.js
@@ -194,7 +194,7 @@ VideoStreamMerger.prototype.addStream = function (mediaStream, opts) {
     videoElement.autoplay = true
     videoElement.muted = true
     videoElement.srcObject = mediaStream
-    videoElement.setAttribute('style', 'position:fixed; left: 0px; top:0px; pointer-events: none; opacity:0; width: 0px; height:0px;')
+    videoElement.setAttribute('style', 'position:fixed; left: 0px; top:0px; pointer-events: none; opacity:0;')
     document.body.appendChild(videoElement)
 
     if (!stream.mute) {
